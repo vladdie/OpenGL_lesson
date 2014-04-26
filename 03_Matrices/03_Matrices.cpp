717cd918cb08f18ba1acf7d34be83686c35e5549
SJQ <sjq597@gmail.com>
2014-04-26 11:59:02 +0800

    第三课
    没写完，先保存一下

--- 03_Matrices/03_Matrices.cpp
+++ 03_Matrices/03_Matrices.cpp
@@ -3,9 +3,132 @@
 
 #include "stdafx.h"
 
+#include <iostream>
+using namespace std;
+
+#include <gl/glew.h>
+
+#include <glfw/glfw3.h>
+GLFWwindow* window;
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+using namespace glm;
+
+#include <common/shader.hpp>
 
 int _tmain(int argc, _TCHAR* argv[])
 {
+	if (!glfwInit())
+	{
+		cerr<<"Failed to initialize GLFW"<<endl;
+		return -1;
+	}
+
+	glfwWindowHint(GLFW_SAMPLES,4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR,4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR,0);
+	glfwWindowHint(GLFW_OPENGL_PROFILE,GLFW_OPENGL_CORE_PROFILE);
+
+	window=glfwCreateWindow(1366,768,"03_Matrices",NULL,NULL);
+	if (window==NULL)
+	{
+		cerr<<"Failed to open GLFW window.Maybe your GPU is not supported!"<<endl;
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+
+	glewExperimental=true;
+	if (glewInit()!=GLEW_OK)
+	{
+		cerr<<"Failed to initialize GLEW!"<<endl;
+		return -1;
+	}
+
+	glfwSetInputMode(window,GLFW_STICKY_KEYS,GL_TRUE);
+
+	glClearColor(0.0f,0.0f,0.4f,0.0f);
+
+	GLuint VertexArrayID;
+	glGenVertexArrays(1,&VertexArrayID);
+	glBindVertexArray(VertexArrayID);
+
+	//Create and compile our GLSL program from the shaders
+	GLuint programID=LoadShaders("SimpleTransform.vertexshader","SimpleColor.fragmentshader");
+
+	//Get a handle for our "MVP" uniform
+	GLuint MatrixID=glGetUniformLocation(programID,"MVP");
+
+	//Projection matrix:45¡ã Field of view,4:3 ratio,display range: 0.1 <-> 100 units
+	glm::mat4 Projection=glm::perspective(45.0f,16.0f/9.0f,0.1f,100.0f);
+
+	//Camera matrix
+	glm::mat4 View=glm::lookAt(
+		glm::vec3(4,3,3),	//Camera is at (4,3,3),in World Space
+		glm::vec3(0,0,0),	//and look at the origin (0,0,0)
+		glm::vec3(0,1,0),	//Head is up (set to 0,-1,0 to look upside-down)
+		);		
+	//Model matrix: an identity matrix (model will be at the origin)
+	glm::mat4 Model= glm::mat4(1.0f);	//Changes for each model!
+
+	//Our ModelViewProjection:multiplication of our 3 matrices
+	glm::mat4 MVP=Projection*View*Model;	//Remember,matrix multiplication is the other way around
+
+	//triangle vertex
+	static const GLfloat gl_vertex_buffer_data[]={
+		-1.0f,-1.0f,0.0f,
+		1.0f,-1.0f,0.0f,
+		0.0f,1.0f,0.0f,
+	};
+	static const GLushort gl_element_buffer_data[]={0,1,2};
+
+	GLuint vertex_buffer;
+	glGenBuffers(1,&vertex_buffer);
+	glBindBuffer(GL_ARRAY_BUFFER,vertex_buffer);
+	glBufferData(GL_ARRAY_BUFFER,sizeof(gl_vertex_buffer_data),gl_vertex_buffer_data,GL_STATIC_DRAW);
+
+	do 
+	{
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		glUseProgram(programID);
+		
+		//Send our transformattion to the currently bound shader
+		//in the "MVP" uniform
+		glUniformMatrix4fv(MatrixID,1,GL_FALSE,&MVP[0][0]);
+		
+		//First attribute buffer:vertices
+		glEnableVertexAttribArray(0);
+		glBindBuffer(GL_ARRAY_BUFFER,vertex_buffer);
+		glVertexAttribPointer(
+			0,	//attribute. No particular reason for 0,but must match the layout in the shader
+			3,	//size
+			GLfloat,	//type
+			GL_FALSE,	//normalized
+			0,	//stride,¼´ÌøÔ¾
+			(void*)0	//array buffer offset
+			);
+
+		//Draw the triangle!
+		glDrawArrays(GL_TRIANGLES,0,3);	//3 indices starting at 0->1 triangle
+
+		glDisableVertexAttribArray(0);
+
+		//Swap buffers
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+
+	} 
+	while (glfwGetKey(window,GLFW_KEY_ESCAPE)!=GLFW_PRESS && glfwWindowShouldClose(window)==0);
+
+	//Cleanup VBO and shader
+	glDeleteBuffers(1,&vertex_buffer);
+	glDeleteProgram(programID);
+	glDeleteVertexArrays(1,&VertexArrayID);
+
+	glfwTerminate();
+
 	return 0;
 }
 
---
+++ 03_Matrices/ReadMe.md
@@ -0,0 +1,32 @@
+﻿    控制台应用程序：03_Matrices 项目概述
+========================================================================
+主要讲了一下OpenGL中的坐标
+
+* 观察坐标系
+* 世界坐标系
+* 模型坐标系
+
+OpenGL中的各种矩阵
+
+* 平移、旋转、缩放
+* 投影矩阵
+* 视图LookAt
+
+C++和GLSL中如何变换
+
+几个函数解释一下
+> - void glUniformMatrix4fv(GLint location,  GLsizei count,  GLboolean transpose,  const GLfloat *value);
+**参数列表：**
+location
+指明要更改的uniform变量的位置
+count
+指明要更改的矩阵个数
+transpose
+指明是否要转置矩阵，并将它作为uniform变量的值,OpenGL ES中必须为GL_FALSE。
+value
+指明一个指向count个元素的指针，用来更新指定的uniform变量。
+**描述：**
+glUniform更改一个uniform变量或数组的值。要更改的uniform变量的位置由location指定，location的值应该由glGetUniformLocation函数返回。通过调用glUseProgram，glUniform操作的程序对象将成为当前状态的一部分。glUniform{1|2|3|4}{f|i}使用传进来的实参，修改通过location指定的uniform变量。所有在程序对象中定义的活动uniform变量，在程序对象链接成功后都会被初始化为0.直到下一次程序对象链接成功再一次被初始化为0前，它们将保留通过调用glUniform赋给它们的值。glUniform{1|2|3|4}{f|i}v可以用来更改单个uniform变量的值，或者一个uniform变量数组。glUniformMatrix{2|3|4}fv用来更改一个矩阵或一个矩阵数组。
+**NOTE:**另外要强调一点的是，glUniformMatrix4fv是默认将数组以列向量的形式存放在矩阵中的，即对于四个向量 (a,b,c,d);(e,f,g,h);(i,j,k, l)；(m, n, o, p)，存放在一个数组中，构成——{a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p}，那么通过调用glUniformMatrix4fv函数，并且第三个参数传递为GL_FALSE，那么获得的矩阵其实是：[a, e, i, m; b, f, j, n; c, g, k, o; d, h, l, p]。如果要将数组中的元素作为行向量传递到	uniform的矩阵中，那么第三个形参transpose应该传给它GL_TRUE值。	
+- void glEnableVertexAttribArray(GLuint index)/void glDisableVertexAttribArray(	GLuint index);
+Specifies the index of the generic vertex attribute to be enabled or disabled.
