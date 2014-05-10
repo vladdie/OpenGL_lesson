30f889118a5415f732bed426a422c20d099d78ce
SJQ <sjq597@gmail.com>
2014-05-10 17:16:18 +0800

    update
    ReadMe.md

--- Test/Test.cpp
+++ Test/Test.cpp
@@ -3,18 +3,13 @@
 #include <iostream>
 using namespace std;
 
-#include <time.h>
+#include <gl/glew.h>
+#include <Windows.h>
+#include <common/loadBMP_custom.hpp>
+
 int main()
 {
-	float test[12*3*3];
-	srand((unsigned)time(NULL));
-	for (int v = 0; v < 12*3 ; v++)
-	{
-		test[3*v+0] =(float) rand()/RAND_MAX;
-		test[3*v+1] =(float) rand()/RAND_MAX;
-		test[3*v+2] =(float) rand()/RAND_MAX;
-		cout<<test[3*v]<<" "<<test[3*v+1]<<" "<<test[3*v+2]<<endl;
-	}
-
+	GLuint image=loadBMP_custom("uvtemplate.bmp");
+	
 	return 0;
 }
